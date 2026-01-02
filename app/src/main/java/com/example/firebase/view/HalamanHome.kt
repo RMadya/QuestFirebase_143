b6ca1b2f92f7fec8570bd81a1ec89ba1d86e4e50
RMadya <kereyoben75@gmail.com>
2026-01-02 22:27:15 +0700

    Bagian ini berfungsi sebagai percabangan logika tampilan berdasarkan status data yang diterima dari ViewModel:
    Loading: Menampilkan gambar/indikator proses jika data sedang diunduh.
    
    Success: Menampilkan daftar siswa jika data berhasil diambil.
    
    Error: Menampilkan pesan kegagalan dan tombol "Retry" untuk mencoba memuat ulang data.

--- app/src/main/java/com/example/firebase/view/HalamanHome.kt
+++ app/src/main/java/com/example/firebase/view/HalamanHome.kt
@@ -71,4 +71,16 @@ fun HomeScreen(
                 )
             }
         },
-    ) }
+    ) { innerPadding ->
+        HomeBody(
+            statusUiSiswa = viewModel.statusUiSiswa,
+            onSiswaClick = navigateToItemUpdate,
+            retryAction = { viewModel.loadSiswa() },
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+
+
