187e8a4e2106e8cbb694511e2b982bc5dd35feb3
RMadya <kereyoben75@gmail.com>
2026-01-08 19:09:20 +0700

    ViewModel standar tidak bisa menerima parameter (seperti Repository atau SavedStateHandle) secara langsung di konstruktornya, kita membutuhkan Factory

--- app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
@@ -1,21 +1,16 @@
 package com.example.firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.firebase.repository.AplikasiDataSiswa
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+                AplikasiDataSiswa
+        )
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
-        }
-        initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
-        }
-    }
-}
