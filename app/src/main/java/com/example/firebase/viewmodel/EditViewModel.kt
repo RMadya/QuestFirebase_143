788e01157586672f76980d151559218ea793be7f
RMadya <kereyoben75@gmail.com>
2026-01-08 18:56:01 +0700

    Mengambil ID siswa dari navigasi.

--- app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.firebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebasetrkotlin.repository.RepositorySiswa
+import com.example.firebase.modeldata.DetailSiswa
+import com.example.firebase.modeldata.UIStateSiswa
+import com.example.firebase.modeldata.toDataSiswa
+import com.example.firebase.modeldata.toUiStateSiswa
+import com.example.firebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    // ✅ FIX: ID STRING (BUKAN LONG)
