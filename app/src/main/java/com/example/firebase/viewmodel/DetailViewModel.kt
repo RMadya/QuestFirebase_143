27bc9280114b952f7735ccd7ef8c11e676c6a7f2
RMadya <kereyoben75@gmail.com>
2026-01-08 18:52:59 +0700

    State Management (Sealed Interface)

--- app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.firebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebasetrkotlin.repository.RepositorySiswa
+import com.example.firebase.modeldata.Siswa
+import com.example.firebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
