5f676215ace56c55c4b9adae4d8379b27ba25c96
RMadya <kereyoben75@gmail.com>
2026-01-08 18:57:44 +0700

    Tujuannya agar data yang ada di ViewModel selalu sinkron dengan apa yang diketik pengguna di layar. final yang mengirimkan data yang sudah diedit kembali ke Firebase.

--- app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
@@ -51,4 +51,17 @@ class EditViewModel(
                 && detailSiswa.telpon.isNotBlank()
     }
 
-    
+    suspend fun editSatuSiswa() {
+        if (validasiInput()) {
+            try {
+                repositorySiswa.editSatuSiswa(
+                    idSiswa,
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
+}
